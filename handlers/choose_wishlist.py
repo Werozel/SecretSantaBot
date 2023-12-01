36a9608e3c5e8307c022076bbc9a0b6714c1e3b2
Werozel <khaibulaev.mn@gmail.com>
2023-12-01 04:01:06 +0300

    Added try/catch

--- handlers/choose_wishlist.py
+++ handlers/choose_wishlist.py
@@ -2,7 +2,9 @@ from telegram import Update
 
 from globals import StateOfPlay
 from models.Player import Player
-from strings import WISHLIST_SELECTED
+from strings import WISHLIST_SELECTED, ANEKI
+
+import random
 
 
 async def choose_wishlist(update: Update, player: Player):
@@ -11,3 +13,5 @@ async def choose_wishlist(update: Update, player: Player):
     player.set_wishlist(wishlist)
     StateOfPlay.save_to_json()
     await update.message.reply_text(WISHLIST_SELECTED)
+    await update.message.reply_text(random.choice(ANEKI))
+
